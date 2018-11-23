2d8cb5369d14367064358e099106206b448871ae
kanishkd4 <kanishkd4@gmail.com>
2018-11-23 10:56:42 +0530

    chap16: traits complete

--- src/bin/c16_traits_iter_clone.rs
+++ src/bin/c16_traits_iter_clone.rs
@@ -42,4 +42,44 @@ fn main() {
     for i in fibonacci().take(4) {
         println!("> {}", i);
     }
+    // The skip method shortens an `Iterator` by dropping its first n terms
+    println!("The next 4 terms of a Fibonacci sequence are: ");
+    for i in fibonacci().skip(4).take(4) {
+        println!("> {}", i);
+    }
+
+    let array = [1u32, 3, 3, 7];
+    println!("Iterate the following array {:?}", &array);
+    for i in array.iter() { // the iter method produces an Iterator over an array/slice
+        println!("> {}", i); 
+    }
+
+    // CLONE
+    let nil = Nil; // Instantiate Nil
+    let copied_nil = nil; // copy nil; there are no resources to move
+    // Both Nil's can be used independently
+    println!("original: {:?}", nil);
+    println!("copy: {:?}", copied_nil);
+
+    let pair = Pair(Box::new(1), Box::new(2)); // instantiate Pair
+    println!("original: {:?}", pair);
+
+    let moved_pair = pair; // Copy pair into moved_pair; moves resources. pair has lost its resources
+    println!("copy: {:?}", moved_pair);
+
+    let cloned_pair = moved_pair.clone();
+    drop(moved_pair); // drop the original pair
+    println!("clone: {:?}", cloned_pair);
 }
+
+// CLONE
+// When dealing with resources, the default behaviour is to transfer them during assignments or function calls.
+// However, sometimes we need to make a copy of the resource as well. 
+// The clone trait helps us do that.
+
+#[derive(Debug, Clone, Copy)] // a unit struct without resources
+struct Nil;
+
+#[derive(Debug ,Clone)]
+struct Pair(Box<i32>, Box<i32>); // a tuple struct with resources that implements the Clone trait
+
